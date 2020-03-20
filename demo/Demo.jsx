fbb2c7f745669c1f702e89c73f8518bd725acda7
martypowell <jpowel25@gmail.com>
2020-03-20 15:01:32 -0400

    added demo files

---
+++ demo/Demo.jsx
@@ -0,0 +1,19 @@
+import Paragraph from "../src/components/P";
+import React from "react";
+
+const Demo = props => {
+  return (
+    <div className="demo">
+      <p>Demo Stuff Goes here</p>
+      <Paragraph>This is some really meaningful text.</Paragraph>
+      <Paragraph>This is some really meaningful text.</Paragraph>
+      <Paragraph>This is some really meaningful text.</Paragraph>
+      <Paragraph>This is some really meaningful text.</Paragraph>
+      <Paragraph>This is some really meaningful text.</Paragraph>
+      <Paragraph>This is some really meaningful text.</Paragraph>
+      <Paragraph>This is some really meaningful text.</Paragraph>
+    </div>
+  );
+};
+
+export default Demo;
---
+++ demo/index.js
@@ -0,0 +1,6 @@
+import Demo from "./Demo";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Demo />, rootElement);
