d68adadbf35e6c253ab7bc6ec7866a0cbf1b433f
martypowell <jpowel25@gmail.com>
2020-03-20 19:44:11 -0400

    added filter list to the demo

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -1,9 +1,11 @@
+import FilterList from "../src/components/FilterList";
 import React from "react";
 
 const Demo = props => {
   return (
     <div className="demo">
       <p>Demo Stuff Goes here</p>
+      <FilterList title="Baltimore County News" />
     </div>
   );
 };
