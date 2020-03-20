88f5a466c45ca795eb74336d256378e5f1e46d0d
martypowell <jpowel25@gmail.com>
2020-03-20 18:24:21 -0400

    removed example component from demo

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -1,17 +1,9 @@
-import Paragraph from "../src/components/P";
 import React from "react";
 
 const Demo = props => {
   return (
     <div className="demo">
       <p>Demo Stuff Goes here</p>
-      <Paragraph>This is some really meaningful text.</Paragraph>
-      <Paragraph>This is some really meaningful text.</Paragraph>
-      <Paragraph>This is some really meaningful text.</Paragraph>
-      <Paragraph>This is some really meaningful text.</Paragraph>
-      <Paragraph>This is some really meaningful text.</Paragraph>
-      <Paragraph>This is some really meaningful text.</Paragraph>
-      <Paragraph>This is some really meaningful text.</Paragraph>
     </div>
   );
 };
