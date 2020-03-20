7d1c4f228cdfd008b794967fb2601f3c32019795
martypowell <jpowel25@gmail.com>
2020-03-20 15:01:07 -0400

    added demo output

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,19 @@ import pkg from "./package.json";
 import resolve from "@rollup/plugin-node-resolve";
 
 export default [
+  {
+    input: "demo/index.js",
+    output: [{ file: pkg.demo, format: "cjs" }],
+    external: ["react", "react-dom"],
+    plugins: [
+      resolve({
+        extensions: [".mjs", ".js", ".jsx", ".json", ".node"]
+      }),
+      babel({
+        exclude: "node_modules/**"
+      })
+    ]
+  },
   // CommonJS (for Node) and ES module (for bundlers) build.
   // (We could have three entries in the configuration array
   // instead of two, but it's quicker to generate multiple
@@ -15,7 +28,7 @@ export default [
       { file: pkg.main, format: "cjs" },
       { file: pkg.module, format: "es" }
     ],
-    external: ["react"],
+    external: ["react", "react-dom"],
     plugins: [
       resolve({
         extensions: [".mjs", ".js", ".jsx", ".json", ".node"]
