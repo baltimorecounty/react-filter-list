08527ff1af7635b40fc6e1b2059fd223b1c15b46
martypowell <jpowel25@gmail.com>
2020-03-21 08:26:12 -0400

    added more pieces to the demo

--- demo/Demo.jsx
+++ demo/Demo.jsx
@@ -1,11 +1,27 @@
 import FilterList from "../src/components/FilterList";
 import React from "react";
 
+const filters = [
+  {
+    targetApiField: "category.value",
+    displayName: "Category",
+    options: [
+      { value: "releases", label: "News Releases" },
+      { value: "stories", label: "Stories" }
+    ]
+  }
+];
+
 const Demo = props => {
   return (
     <div className="demo">
       <p>Demo Stuff Goes here</p>
-      <FilterList title="Baltimore County News" />
+      <FilterList
+        title="Baltimore County News"
+        filters={filters}
+        apiEndpoint="https://structuredcontentdev.bcg.ad.bcgov.us/api/news"
+        renderItem={({ title }) => <div>{title}</div>}
+      />
     </div>
   );
 };
