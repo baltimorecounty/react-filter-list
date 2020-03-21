7a9e036e7d1ecb29635f224fe43c78eb2ff24b08
martypowell <jpowel25@gmail.com>
2020-03-21 12:46:38 -0400

    rolled back prior to laod more

--- src/components/ApiList.jsx
+++ src/components/ApiList.jsx
@@ -1,20 +1,14 @@
 import "promise-polyfill/src/polyfill";
 import "whatwg-fetch";
 
-import React, { useState } from "react";
-
+import React from "react";
 import RecordsMessage from "./RecordsMessage";
 import { useQuery } from "react-query";
 
 const fetchList = (key, { endpoint }) =>
   fetch(endpoint).then(res => res.json());
 
-const ApiList = ({
-  title,
-  endpoint: endpointFromProps,
-  renderItem = () => {}
-}) => {
-  const [endpoint, setEndpoint] = useState(endpointFromProps);
+const ApiList = ({ title, endpoint, renderItem = () => {} }) => {
   const { data, error } = useQuery(["apiGET", { endpoint }], fetchList);
 
   if (!data) {
@@ -25,21 +19,13 @@ const ApiList = ({
     return <p>Something went wrong loading {title}.</p>;
   }
 
-  const {
-    records = [],
-    metaData: { links: { next } = {}, totalRecords = 0 } = {}
-  } = data;
-
-  const handleMoreClick = () => {
-    setEndpoint(next);
-  };
+  const { records = [], metaData: { totalRecords = 0 } = {} } = data;
 
   return (
     <>
       <RecordsMessage count={totalRecords} />
       <div className="items">{records.map(renderItem)}</div>
       <RecordsMessage count={totalRecords} />
-      {next && <button onClick={handleMoreClick}>Load More</button>}
     </>
   );
 };
