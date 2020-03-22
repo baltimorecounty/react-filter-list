3da705e06658b68d6600bd0e36c56efbf4e3eec3
martypowell <jpowel25@gmail.com>
2020-03-22 10:58:50 -0400

    added proptypes to filter function

--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import ApiList from "./ApiList.jsx";
 import Filters from "./Filters.jsx";
+import PropTypes from "prop-types";
 import { UpdateQueryString } from "../common/Filters";
 
 const FilterList = ({
@@ -40,6 +41,20 @@ const FilterList = ({
       </div>
     </div>
   );
+};
+
+FilterList.propTypes = {
+  /** Name of the collection of data, Ex. Baltimore County News */
+  title: PropTypes.string.isRequired,
+  /**
+   * Function to display a single list item.
+   * The function is provided  all available information about that item.
+   */
+  renderItem: PropTypes.func.isRequired,
+  /** List of filters. A filter contains `targetApiField`, `displayName`, and list of options { label, value } */
+  filters: PropTypes.array.isRequired,
+  /** Fully qualified api url plus endpoint targeting for the list. Ex. https://mycoolsite/api/news  */
+  apiEndpoint: PropTypes.string.isRequired
 };
 
 export default FilterList;
