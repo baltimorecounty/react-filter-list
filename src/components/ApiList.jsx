3df0373a7db3c1e755c8015b0252dcad192e62b8
martypowell <jpowel25@gmail.com>
2020-04-15 10:28:55 -0400

    🐛 FIX: maintian scroll position on load more click

--- src/components/ApiList.jsx
+++ src/components/ApiList.jsx
@@ -42,12 +42,16 @@ const ApiList = ({
     }
   );
 
-  if (isFetching) {
+  if (status === "loading") {
     return <p>Loading {title}...</p>;
   }
 
-  if (error) {
-    return <p>Something went wrong loading {title}.</p>;
+  if (status === "error") {
+    return (
+      <p>
+        Something went wrong with {title}. Please try again in a few minutes.
+      </p>
+    );
   }
 
   const { metaData: { totalRecords = 0 } = {} } = data[0] || {};
