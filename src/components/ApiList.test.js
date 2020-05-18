b32ec2dc4fa8568cf35f592bbda3d05897c405dc
Marty Powell <jpowel25@gmail.com>
2020-05-17 21:02:53 -0400

    added basic test

--- src/components/ApiList.test.js
+++ src/components/ApiList.test.js
@@ -3,19 +3,44 @@ import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import ApiList from "./ApiList";
+import { FetchList as mockFetchList } from "../common/Fetch";
+
+jest.mock("../common/Fetch");
+
+const MockNewsCard = ({ title, author }) => (
+  <div>
+    <h1>{title}</h1>
+    <p>{author}</p>
+  </div>
+);
 
 test("loads and displays a list", async () => {
-  render(<ApiList title="My List" />);
+  mockFetchList.mockResolvedValueOnce({
+    metaData: { totalRecords: 2 },
+    records: [
+      { title: "News Story 1", author: "Jane Doe" },
+      { title: "News Story 2", author: "John Doe" },
+    ],
+  });
+
+  render(
+    <ApiList
+      title="My List"
+      renderItem={(props) => (
+        <MockNewsCard key={props.title + props.author} {...props} />
+      )}
+    />
+  );
 
   expect(screen.getByText(/loading my list.../i)).toBeInTheDocument();
 
-  //   axiosMock.get.mockResolvedValueOnce({
-  //     data: { greeting: "hello there" },
-  //   });
-  //   fireEvent.click(screen.getByText("Load Greeting"));
-  //   await waitFor(() => screen.getByRole("heading"));
-  //   expect(axiosMock.get).toHaveBeenCalledTimes(1);
-  //   expect(axiosMock.get).toHaveBeenCalledWith(url);
-  //   expect(screen.getByRole("heading")).toHaveTextContent("hello there");
-  //   expect(screen.getByRole("button")).toHaveAttribute("disabled");
+  expect(mockFetchList).toBeCalledTimes(1);
+
+  await waitFor(() => {
+    expect(screen.getByText(/2 result/i)).toBeInTheDocument();
+    expect(screen.getByText(/news story 1/i)).toBeInTheDocument();
+    expect(screen.getByText(/jane doe/i)).toBeInTheDocument();
+    expect(screen.getByText(/news story 2/i)).toBeInTheDocument();
+    expect(screen.getByText(/john doe/i)).toBeInTheDocument();
+  });
 });
