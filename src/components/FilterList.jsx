93f0d9d043e6ed56aae833386bb8e9d02e7af0da
sgurung <sgurung@OIT228469>
2020-07-14 08:09:46 -0400

    test

--- src/components/FilterList.jsx
+++ src/components/FilterList.jsx
@@ -141,7 +141,7 @@ const FilterList = ({
     <div {...props}>
       <div className="row">
         <div className="col-md-3 col-xs-12">
-          <DatePicker
+          <DateSelector
             name={fromDateId}
             id={fromDateId}
             selected={fromDate}
