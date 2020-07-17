52a899072ddcbd384bb0823cdf934e6718346d84
sgurung <sgurung@OIT228469>
2020-07-17 08:03:02 -0400

    removed code

--- demo/index.js
+++ demo/index.js
@@ -1,7 +1,6 @@
 import Demo from "./Demo";
 import React from "react";
 import ReactDOM from "react-dom";
-//import "react-datepicker/dist/react-datepicker.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<Demo />, rootElement);
