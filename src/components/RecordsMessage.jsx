2f3da2190a20f317b1f04561008ef4c548e7664a
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-07-28 08:26:18 -0400

    Fixed message

--- src/components/RecordsMessage.jsx
+++ src/components/RecordsMessage.jsx
@@ -4,8 +4,8 @@ const RecordsMessage = ({
   count = 0,
   renderMessage = ({ count }) =>
     count === 0
-      ? `View ${count} results. Try updating your filter criteria`
-      : `View ${count} results`,
+      ? `Viewing ${count} results. Try updating your filter criteria`
+      : `Viewing ${count} results`,
   ...rest
 }) => {
   return <p {...rest}>{renderMessage({ count })}</p>;
