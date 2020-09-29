4707081fb0f5f47e54bd55e9aa9bbc165eb26f73
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-09-29 13:24:20 -0400

    Formatting

--- src/common/Filters.js
+++ src/common/Filters.js
@@ -1,9 +1,9 @@
 import { parse } from "query-string";
 import { subMonths } from "date-fns";
 /** Resets filter to default state. All options are unchecked */
-const resetFilter = filter => {
+const resetFilter = (filter) => {
   const { options = [] } = filter;
-  options.map(option => {
+  options.map((option) => {
     option.checked = false;
     return option;
   });
@@ -28,7 +28,7 @@ const updateFilters = (filters = [], queryStringFilters = {}) => {
   // Reset any filters that do not exist in the current querystring
   resetEmptyFilters(filters, queryStringFilters);
   // Update active filters based on querystring
-  Object.keys(queryStringFilters).forEach(key => {
+  Object.keys(queryStringFilters).forEach((key) => {
     const matchingFilter = filters.find(
       ({ targetApiField = "" }) =>
         targetApiField.toLowerCase() == key.toLowerCase()
@@ -38,7 +38,7 @@ const updateFilters = (filters = [], queryStringFilters = {}) => {
       const urlValues = queryStringFilters[key].toLowerCase().split(",");
       const { options = [] } = matchingFilter;
 
-      options.map(option => {
+      options.map((option) => {
         const { value = "" } = option;
         option.checked = urlValues.some(
           (urlValue = "") => value.toLowerCase() === urlValue.toLowerCase()
@@ -73,7 +73,7 @@ const UpdateUrlQueryString = (url, name, value) => {
  *
  * @param {date} date for data value
  */
-const FormatDateString = date => {
+const FormatDateString = (date) => {
   var formatDateValue =
     `${date.getMonth() + 1}` +
     `/` +
@@ -84,13 +84,13 @@ const FormatDateString = date => {
   return formatDateValue;
 };
 
-const ShowHideSmallSizeCheckBox = checkboxName => {
+const ShowHideSmallSizeCheckBox = (checkboxName) => {
   var species = ["cat", "other"];
   var checkboxes = document.querySelectorAll(
       'input[name="' + checkboxName + '"]:checked'
     ),
     values = [];
-  Array.prototype.forEach.call(checkboxes, function(el) {
+  Array.prototype.forEach.call(checkboxes, function (el) {
     values.push(el.value.toUpperCase());
   });
 
@@ -186,7 +186,7 @@ const UpdateFilters = (filters = [], queryString = "") => {
  */
 const UpdateQueryString = ({
   filter: { checked, name, value },
-  queryString
+  queryString,
 }) => {
   const searchParams = new URLSearchParams(queryString || "");
   const existingValues = searchParams.has(name)
@@ -226,9 +226,9 @@ const UpdateQueryString = ({
 const FilterSearchTags = (searchTags = [], textQuery = "", searchCategory) => {
   let newQuery = "";
 
-  searchTags[searchCategory].map(item => {
+  searchTags[searchCategory].map((item) => {
     const tagCategory = item.value.split(",");
-    tagCategory.map(tag => {
+    tagCategory.map((tag) => {
       if (tag.toLowerCase().trim() === textQuery.toLowerCase().trim()) {
         newQuery = item.label;
       }
@@ -245,5 +245,5 @@ export {
   FormatDateString,
   InitilizeDateValues,
   ShowHideSmallSizeCheckBox,
-  FilterSearchTags
+  FilterSearchTags,
 };
